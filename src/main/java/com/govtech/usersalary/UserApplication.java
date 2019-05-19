6823bb0a3c75ca53592e9bebe4e651b81ea76e67
Joan Tan <joan.tan.xy@gmail.com>
2019-05-19 17:00:34 +0800

    Changed build method to war in build.gradle & main added example csv file added built .war file

--- src/main/java/com/govtech/usersalary/UserApplication.java
+++ src/main/java/com/govtech/usersalary/UserApplication.java
@@ -2,9 +2,16 @@ package com.govtech.usersalary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserApplication {
+public class UserApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
